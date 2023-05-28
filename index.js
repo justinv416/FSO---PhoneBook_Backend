161fdc5819f7c62c9ab26e41516d8a3bf7555680
justinv416 <vien_justin@hotmail.com>
2023-05-28 12:02:32 -0400

    completed ex. 3.1 - 3.2

--- index.js
+++ index.js
@@ -27,6 +27,11 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 });
 
+app.get('/info', (request, response) => {
+    const date = new Date()
+    response.send(`Phonebook has info for ${persons.length} people <br/><br/> ${date}`)
+})
+
 const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
